77203f81c4f54f7c05f0e976f61e9df39882133f
Algorithm79 <magnus.widegren@gmail.com>
2019-10-01 14:08:12 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,13 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
+# Launch Polybar, using default config location ~/.config/polybar/config
 polybar monitor1 &
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload monitor1 &
-  done
-else
-  polybar --reload monitor1 &
-fi
+echo "Polybar launched..."
