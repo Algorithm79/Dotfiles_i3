3639842bd7b2c7e03a91c332e1f0fbf77f0a672f
Algorithm79 <magnus.widegren@gmail.com>
2020-07-20 12:37:51 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar monitor1 &
+polybar -rq bar &
 
 echo "Polybar launched..."
