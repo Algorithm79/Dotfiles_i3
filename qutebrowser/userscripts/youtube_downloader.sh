505c158a066c93a63430d8230dc3c39bccdbdcde
Algorithm79 <magnus.widegren@gmail.com>
2021-07-01 16:16:13 +0200

    Delete qutebrowser directory

--- qutebrowser/userscripts/youtube_downloader.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(youtube-dl --output "$QUTE_DOWNLOAD_DIR/%(title)s.%(ext)s" $QUTE_URL && 
-    notify-send "YouTube download completed!" "$QUTE_TITLE") || exit 0
--- qutebrowser/userscripts/youtube_mp3_downloader.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-(youtube-dl --output "$QUTE_DOWNLOAD_DIR/%(title)s.%(ext)s" -x --audio-format mp3 --audio-quality 0 $QUTE_URL && 
-    notify-send "YouTube mp3 download completed!" "$QUTE_TITLE") || exit 0
